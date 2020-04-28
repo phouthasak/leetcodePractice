be0b57796ac80ac60bb01868a817b88c0b18fcf1
phouthasak douanglee <jphouthasakdouanglee@gmail.com>
2020-04-28 00:16:07 -0400

    Added rotate array

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
     //runner class
     public static void main(String[] args) {
+        RotateArray rotateArray = new RotateArray();
+        int[] input = new int[] {0,1,2};
+        int k = 1;
 
+        rotateArray.rotate(input, k);
+        for(int value : input) {
+            System.out.println(value);
+        }
     }
 }
